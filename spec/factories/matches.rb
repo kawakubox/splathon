700b0b3de053914ece8efa10242f1b11d0ab06e8
kawakubox <shigefumi.kawakubo@gmail.com>
2017-10-25 01:31:12 +0900

    Rename match result (#13)
    * rename file
    
    * Rename class
    
    * Raname table

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :match_result do
+  factory :match do
     team
     opponent { FactoryBot.create(:team) }
     points { [0, 1, 3].sample }
