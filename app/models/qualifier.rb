30f5e3119ba47fdf79284027f757596626748917
kawakubox <shigefumi.kawakubo@gmail.com>
2017-10-29 12:48:45 +0900

    Add Qualifier.next_round method

--- app/models/qualifier.rb
+++ app/models/qualifier.rb
@@ -4,4 +4,8 @@ class Qualifier < ApplicationRecord
   belongs_to :event
   has_many   :qualifier_matches, dependent: :destroy
   has_many   :matches, through: :qualifier_matches
+
+  def self.next_round(event_id)
+    Qualifier.where(event_id: event_id).maximum(:round).to_i + 1
+  end
 end
--- spec/models/qualifier_spec.rb
+++ spec/models/qualifier_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Qualifier, type: :model do
   it { is_expected.to respond_to :round }
   it { is_expected.to respond_to :order_of_match }
   it { is_expected.to respond_to :matches }
+
+  describe '.next_round' do
+    let(:event) { FactoryBot.create(:event) }
+    context '予選ラウンドが存在しない場合' do
+      it '1 であること' do
+        expect(Qualifier.next_round(event.id)).to eq 1
+      end
+    end
+
+    context '予選ラウンド1が存在する場合' do
+      it '2 であること' do
+        FactoryBot.create(:qualifier, event: event, round: 1)
+        expect(Qualifier.next_round(event.id)).to eq 2
+      end
+    end
+  end
 end
