9d4dc0fa55b80475de9f23713effedd69211cbab
kawakubox <shigefumi.kawakubo@gmail.com>
2017-11-01 07:39:57 +0900

    裏の組み合わせを取得するメソッド

--- app/models/qualifier_match.rb
+++ app/models/qualifier_match.rb
@@ -3,4 +3,8 @@
 class QualifierMatch < ApplicationRecord
   belongs_to :qualifier
   belongs_to :match, dependent: :destroy
+
+  def back_combination
+    QualifierMatch.where(qualifier: qualifier, order_of_match: order_of_match).where.not(id: id).first
+  end
 end
