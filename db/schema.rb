7b07de23784bfc853d8e19b4ee06bf86b36e1e16
kawakubox <shigefumi.kawakubo@gmail.com>
2017-12-31 06:50:06 +0900

    Generate the Room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210135233) do
+ActiveRecord::Schema.define(version: 20171230213829) do
 
   create_table "events", force: :cascade do |t|
     t.date "held_on"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20171210135233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_qualifiers_on_event_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "event_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_rooms_on_event_id"
   end
 
   create_table "teams", force: :cascade do |t|
---
+++ spec/models/room_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  subject { FactoryBot.build(:room) }
+end
