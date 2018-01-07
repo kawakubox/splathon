8f22eefe3bbdbd80389ec702bafa9a0844e4236c
kawakubox <shigefumi.kawakubo@gmail.com>
2018-01-07 09:52:24 +0900

    最終戦の優先する処理をコメントアウト

--- app/services/room_allocator.rb
+++ app/services/room_allocator.rb
@@ -18,7 +18,8 @@ class RoomAllocator
                         .reverse
 
     # 初戦を除きあとのほうがより強いチームになるので、優先度高い部屋を割り当てるようにする
-    @matches.last.update!(room: room_stocks.shift)
+    # なんか偏る傾向あるので、ここはコメントアウト
+    # @matches.last.update!(room: room_stocks.shift)
 
     # 残りの試合は、優先度高い部屋の利用が少ないチームが割り当てられるようにする
     @matches.where(room: nil).sort { |a, b| a.room_priority <=> b.room_priority }.each do |match|
