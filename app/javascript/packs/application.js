7ad5f525d5d2c7d30a7e4b69199d08f5388f25c2
kawakubox <shigefumi.kawakubo@gmail.com>
2018-04-16 02:07:40 +0900

    asset を webpacker に寄せる (#51)
    * Add yarn.lock
    * asset を webpacker に寄せる
    * Delete a app/assets directory
    * Remove `public/assets/manifest-*.json` file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import 'jquery'
+import 'bootstrap'
 
-console.log('Hello World from Webpacker')
+import 'bootstrap/dist/css/bootstrap'
+import 'font-awesome/css/font-awesome'
+import 'admin-lte/dist/css/AdminLTE'
+import 'admin-lte/dist/css/skins/skin-green'
+import '../src/events.scss'
+import '../src/qualifiers.scss'
+import '../src/teams.scss'
