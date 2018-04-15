2b323a49f7fe22475890720247842381c56a9fa7
kawakubox <shigefumi.kawakubo@gmail.com>
2018-04-16 00:14:34 +0900

    Load jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,13 @@
 const { environment } = require('@rails/webpacker');
 
 const webpack = require('webpack');
-// environment.plugins.set('Provide', new webpack.ProvidePlugin({
-//   $: 'jquery',
-//   jQuery: 'jquery',
-// }));
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+);
 
 module.exports = environment;
