e5e29f968fa17608cc6092f2e68555f55c127e81
kawakubox <shigefumi.kawakubo@gmail.com>
2019-02-20 00:40:26 +0900

    (wip) viewテンプレート切り替えを実装
    [ci skip]

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'browser'
 gem "font-awesome-rails"
 gem 'pg'
 gem 'webpacker'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_variant
+
+  def set_variant
+    request.variant = browser.device.mobile? ? :sp : ''
+  end
 end
