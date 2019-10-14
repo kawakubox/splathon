cc49cd59266296459277a41c20742806a724a5d8
kawakubox <shigefumi.kawakubo@gmail.com>
2019-10-14 11:42:21 +0900

    Fix Faker::Date.between usage

--- spec/factories/slack_billing_changes.rb
+++ spec/factories/slack_billing_changes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :slack_billing_change do
-    detected_on { Faker::Date.between(1.year.ago, Date.today) }
+    detected_on { Faker::Date.between(from: 1.year.ago, to: Date.today) }
     member_id { 'U' + Faker::Alphanumeric.alphanumeric(8).upcase }
     display_name { Faker::Name.name }
     action { Faker::Lorem.sentence }
