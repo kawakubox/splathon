f20273d90e762c522c413f341c257b0aa5f1eb1e
kawakubox <shigefumi.kawakubo@gmail.com>
2019-10-14 11:34:41 +0900

    Fix some specs

--- spec/models/slack_billing_change_message_spec.rb
+++ spec/models/slack_billing_change_message_spec.rb
@@ -8,37 +8,37 @@ RSpec.describe SlackBillingChangeMessage do
 
     context 'when joined the workspace' do
       let(:message) { 'かわくぼっくす at Speee joined the workspace (regular account invited by まじん 元speee)' }
-      its(:account_name) { is_expected.to eq 'かわくぼっくす at Speee' }
+      its(:display_name) { is_expected.to eq 'かわくぼっくす at Speee' }
       its(:action) { is_expected.to eq 'joined' }
     end
 
     context 'when detected as active' do
       let(:message) { 'Tenpin at NRI was detected as active' }
-      its(:account_name) { is_expected.to eq 'Tenpin at NRI' }
+      its(:display_name) { is_expected.to eq 'Tenpin at NRI' }
       its(:action) { is_expected.to eq 'active' }
     end
 
     context 'when detected as inactive' do
       let(:message) { 'にっしー at Spee was detected as inactive' }
-      its(:account_name) { is_expected.to eq 'にっしー at Spee' }
+      its(:display_name) { is_expected.to eq 'にっしー at Spee' }
       its(:action) { is_expected.to eq 'inactive' }
     end
 
     context 'when deactivate' do
       let(:message) { 'Oda .Cupsule.LLC was deactivated by かわくぼっくす at Speee' }
-      its(:account_name) { is_expected.to eq 'Oda .Cupsule.LLC' }
+      its(:display_name) { is_expected.to eq 'Oda .Cupsule.LLC' }
       its(:action) { is_expected.to eq 'deactivated' }
     end
 
     context 'when became xxx' do
       let(:message) { 'ろーる at BizReach became an Administrator (promoted by みーくん at SGN/SGC)' }
-      its(:account_name) { 'ろーる at BizReach' }
+      its(:display_name) { 'ろーる at BizReach' }
       its(:action) { 'be Administrator' }
     end
 
     context 'when is no longer xxx' do
       let(:message) { 'まけトピア (東京大学 / ex-dwango) is no longer an Administrator (permission changed by みーくん at SGN/SGC)' }
-      its(:account_name) { is_expected.to eq 'まけトピア (東京大学 / ex-dwango)' }
+      its(:display_name) { is_expected.to eq 'まけトピア (東京大学 / ex-dwango)' }
       its(:action) { is_expected.to eq 'no longer Administrator' }
     end
   end
